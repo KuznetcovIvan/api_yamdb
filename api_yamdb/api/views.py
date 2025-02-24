2e4304c3dfeb598708d67683548c2d3dc8c143a4
Anastasiia Kharkova <ksipsi@gmail.com>
2025-02-24 19:44:30 +0700

    Первая версия моделей, view и эндпойнтов

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,34 @@
+from rest_framework import viewsets, permissions, filters
+from rest_framework.pagination import PageNumberPagination
+from .models import Title, Category, Genre
+from .serializers import TitleSerializer, CategorySerializer, GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """Вьюсет для произведений."""
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = PageNumberPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """Вьюсет для категорий."""
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'slug'
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    """Вьюсет для жанров."""
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    lookup_field = 'slug'
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
