5368329ad5af7a249ec70d03807022263bbdc6f7
Anastasiia Kharkova <ksipsi@gmail.com>
2025-03-01 14:02:16 +0700

    пермишен

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,14 +6,8 @@ class IsAdmin(BasePermission):
         return request.user.is_authenticated and request.user.is_admin()
 
 
-class IsAdminOrModeratorOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS or request.user.is_authenticated
-                and request.is_moderator_or_admin())
-
-
-class IsAuthorModeratorAdminOrReadOnly(BasePermission):
-    def has_object_permission(self, request, view, obj):
+class IsAdminModeratorAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj): 
         return (request.method in SAFE_METHODS or obj.author == request.user
                 or request.user.is_moderator_or_admin())
 
